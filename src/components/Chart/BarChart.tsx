2a932372737f527dc05751d48e046dd76d425133
ducndt-dgg <ducnd@dgg.network>
2023-02-01 22:47:47 +0700

    Fix chart

--- src/components/Chart/BarChart.tsx
+++ src/components/Chart/BarChart.tsx
@@ -1,5 +1,8 @@
 import React from "react"
 import { Bar } from "react-chartjs-2"
+import Chart from "chart.js/auto"
+import { CategoryScale } from "chart.js"
+Chart.register(CategoryScale)
 
 const data = {
   labels: [
